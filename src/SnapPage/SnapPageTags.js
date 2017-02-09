0db2dbd014fc63a990ab6872770b3dd0cba62d1f
Vesa Rautiainen <vrautiainen@gmail.com>
2017-02-09 17:45:23 +0000

    Tags layout change

--- src/SnapPage/SnapPageTags.js
+++ src/SnapPage/SnapPageTags.js
@@ -4,9 +4,10 @@ export default function SnapPageTags(props) {
   return (
     <div className='SnapPageTags'>
       <p>
+        <span style={{marginRight: '15px'}}><a role='button'>store {'\u203A'}</a></span>
         {props.tags.map((tagname, i) => (
           <span key={i}>
-            {i? <span>{', '}</span> : null}
+            {i? <span style={{marginRight: '15px'}}></span> : null}
             <a role='button'>{tagname}</a>
           </span>
         ))}
